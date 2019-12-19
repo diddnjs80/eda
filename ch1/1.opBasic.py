7446d59e37f5fca737b4f90262e6d491aeffb471
kang yangwon <yw.kang.green@gmail.com>
2019-12-19 14:59:27 +0900

    ch1-1

---
+++ ch1/1.opBasic.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 14:00:11 2019
+
+@author: Yang
+"""
+
+a = 10
+#%%
+b = 20.0
+c = a+b
+#%%
+print(a)
+#%%
+print(b)
+print(c)
+#%%
+age = 17
+h = 178.5
+print('input name: ')
+name = input()
+print(name)
+print('your name is ',name)
+print('your name is ' + name)
+print('your name is %s,%d,%f' % (name, age , h))
+#%%
+name = input('Input name :')
+print('your name is' + name)
